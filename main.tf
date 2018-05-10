fcacd0cb4a0fb5851458c6fcf684c9f56d580b06
root <root@lnx-vm071.homelab.int>
2018-05-10 23:58:34 +0200

    Added user_data

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ resource "aws_instance" "example" {
 	ami = "ami-f4f21593"
 	instance_type = "t2.micro"
 
+	user_data = <<-EOF
+        	    #!/bin/bash              
+		    echo "Hello, World" > index.html              
+		    nohup busybox httpd -f -p 8080 &              
+		    EOF
+
 	tags {
 	Name = "terraform-example"
 	}

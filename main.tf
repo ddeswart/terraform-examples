c288d80e62ae68da685dba5f2dbde19b70292fd5
root <root@lnx-vm071.homelab.int>
2018-05-12 00:30:55 +0200

    Changed format of tf file

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-	region = "eu-west-2"
+  region = "eu-west-2"
 }
 
 resource "aws_instance" "example" {
-	ami = "ami-c12dcda6"
-	instance_type = "t2.micro"
-	vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  ami                    = "ami-c12dcda6"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
-	user_data = <<-EOF
+  user_data = <<-EOF
 		    #!/bin/bash
 		    sudo yum update -y
 		    sudo yum install httpd -y
@@ -18,25 +18,25 @@ resource "aws_instance" "example" {
 		    sudo echo "Hello Cloudgurus!" > index.html
 		    EOF
 
-	tags {
-	Name = "terraform-example"
-	}
+  tags {
+    Name = "terraform-example"
+  }
 }
 
 resource "aws_security_group" "instance" {
-	name = "terraform-example-instance"
+  name = "terraform-example-instance"
 
-	ingress {
-	from_port = 80
-	to_port = 80
-	protocol = "tcp"
-	cidr_blocks = ["0.0.0.0/0"]
-	}
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-	egress {
-	from_port = 0
-	to_port = 0
-	protocol = "-1"
-	cidr_blocks = ["0.0.0.0/0"]
-	}
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }

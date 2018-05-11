c30793ce6828e77e42cdc7c81b755f9e96de25e5
root <root@lnx-vm071.homelab.int>
2018-05-11 21:44:22 +0200

    Added Security Group

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
 	ami = "ami-f4f21593"
 	instance_type = "t2.micro"
+	vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
 	user_data = <<-EOF
         	    #!/bin/bash              
@@ -14,5 +15,16 @@ resource "aws_instance" "example" {
 
 	tags {
 	Name = "terraform-example"
+	}
+}
+
+resource "aws_security_group" "instance" {
+	name = "terraform-example-instance"
+
+	ingress {
+	from_port = 8080
+	to_port = 8080
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
 	}
 }

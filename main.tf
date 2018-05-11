fba5cfe520510657c2e2008fa1d33a4234a8ff6b
root <root@lnx-vm071.homelab.int>
2018-05-12 00:07:31 +0200

    Changed User_data and Security Group settings

--- main.tf
+++ main.tf
@@ -3,14 +3,19 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-	ami = "ami-f4f21593"
+	ami = "ami-c12dcda6"
 	instance_type = "t2.micro"
 	vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
 	user_data = <<-EOF
-        	    #!/bin/bash              
-		    echo "Hello, World" > index.html              
-		    nohup busybox httpd -f -p 8080 &              
+		    #!/bin/bash
+		    sudo yum update -y
+		    sudo yum install httpd -y
+		    sudo service httpd start
+		    sudo chkconfig httpd on
+		    cd /var/www/html
+		    sudo touch index.html
+		    sudo echo "Hello Cloudgurus!" > index.html
 		    EOF
 
 	tags {
@@ -22,9 +27,16 @@ resource "aws_security_group" "instance" {
 	name = "terraform-example-instance"
 
 	ingress {
-	from_port = 8080
-	to_port = 8080
+	from_port = 80
+	to_port = 80
 	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	egress {
+	from_port = 0
+	to_port = 0
+	protocol = "-1"
 	cidr_blocks = ["0.0.0.0/0"]
 	}
 }

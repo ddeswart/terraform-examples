b53ab78b524d906550ecc2cd03c266b7696e7b83
root <root@lnx-vm071.homelab.int>
2018-05-25 15:40:33 +0200

    Added DynamoDB for State locking

--- terraform-state/main.tf
+++ terraform-state/main.tf
@@ -2,15 +2,18 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+## Backend ##
 terraform {
   backend "s3" {
     bucket  = "mycloudlabbucket-terraform-state"
     key     = "terraform.tfstate"
     region  = "eu-west-2"
     encrypt = true
+    dynamodb_table = "terraform-state-lock"
   }
 }
 
+## Resources for Backend ##
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "mycloudlabbucket-terraform-state"  
   
@@ -20,6 +23,22 @@ resource "aws_s3_bucket" "terraform_state" {
   
   lifecycle {    
     prevent_destroy = true  
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_statelock" {
+  name           = "terraform-state-lock"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
 

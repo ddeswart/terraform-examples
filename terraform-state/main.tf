507e32af0792f300bb0cb5ad424fa1df0c96bd63
root <root@lnx-vm071.homelab.int>
2018-05-25 15:54:52 +0200

    Placed Backend config at end of file

--- terraform-state/main.tf
+++ terraform-state/main.tf
@@ -2,17 +2,6 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-## Backend ##
-terraform {
-  backend "s3" {
-    bucket  = "mycloudlabbucket-terraform-state"
-    key     = "terraform.tfstate"
-    region  = "eu-west-2"
-    encrypt = true
-    dynamodb_table = "terraform-state-lock"
-  }
-}
-
 ## Resources for Backend ##
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "mycloudlabbucket-terraform-state"  
@@ -39,6 +28,17 @@ resource "aws_dynamodb_table" "terraform_statelock" {
 
   lifecycle {
     prevent_destroy = true
+  }
+}
+
+## Backend ##
+terraform {
+  backend "s3" {
+    bucket  = "mycloudlabbucket-terraform-state"
+    key     = "terraform.tfstate"
+    region  = "eu-west-2"
+    encrypt = true
+    dynamodb_table = "terraform-state-lock"
   }
 }
 

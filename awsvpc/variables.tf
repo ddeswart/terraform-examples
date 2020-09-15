43ebec2adb425d2c573abe5ba78cad35a0687e8d
ddeswart <ddeswart@vmware.com>
2020-09-15 14:22:21 +0200

    Added awsvpc

---
+++ awsvpc/variables.tf
@@ -0,0 +1,46 @@
+# Input variable definitions
+
+variable "vpc_name" {
+  description = "Name of VPC"
+  type        = string
+  default     = "example-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "172.10.0.0/16"
+}
+
+variable "vpc_azs" {
+  description = "Availability zones for VPC"
+  type        = list
+  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+}
+
+variable "vpc_private_subnets" {
+  description = "Private subnets for VPC"
+  type        = list(string)
+  default     = ["172.10.1.0/24", "172.10.2.0/24"]
+}
+
+variable "vpc_public_subnets" {
+  description = "Public subnets for VPC"
+  type        = list(string)
+  default     = ["172.10.10.0/24", "172.10.20.0/24"]
+}
+
+variable "vpc_enable_nat_gateway" {
+  description = "Enable NAT gateway for VPC"
+  type    = bool
+  default = true
+}
+
+variable "vpc_tags" {
+  description = "Tags to apply to resources created by VPC module"
+  type        = map(string)
+  default     = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}

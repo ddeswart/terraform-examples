39eea4b4791c027b67f428c1eec23c0e223ab512
ddeswart <ddeswart@vmware.com>
2020-09-21 12:14:19 +0200

    Changed to element

--- awsvpc/main.tf
+++ awsvpc/main.tf
@@ -25,7 +25,7 @@ data "aws_availability_zones" "available" {}
 resource "aws_subnet" "public" {
   count                   = length(data.aws_availability_zones.available.names)
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.subnet_cidr[count.index]
+  cidr_block              = element(var.subnet_cidr, count.index)
   map_public_ip_on_launch = true
   availability_zone       = element(data.aws_availability_zones.available.names, count.index)
 }
